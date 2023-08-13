385ceff1f243d484d71669e652196f8f69dcdf33
Veronica_Olivera <ve.olivera@hotmail.com>
2023-08-13 16:29:21 -0300

    New branch

--- CatAutos/workspace/workspace/js/products.js
+++ CatAutos/workspace/workspace/js/products.js
@@ -1,1 +1,19 @@
 const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+const container = document.getElementById("container");
+
+/*function showData(dataArray) {
+    for (const item of dataArray) {
+      container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; 
+    }
+  }
+  
+  async function pedidoURL() {
+    let response = await fetch(DATA_URL);
+    let json = await response.json();
+    showData(json.students);
+  }
+  
+  pedidoURL();
+
+  
