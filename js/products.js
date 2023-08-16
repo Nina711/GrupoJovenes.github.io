a891032bb9306d3954232393a9eb230f8e211563
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-08-16 19:54:24 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -42,7 +42,7 @@ function showProductsList(){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">${prod[i].name + " - " + prod[i].currency +" "+ prod[i].cost}</h4>
-                        <small class="text-muted">${prod[i].soldCount} artículos</small>
+                        <small class="text-muted">${prod[i].soldCount} artículos disponibles</small>
                     </div>
                     <p class="mb-1">${prod[i].description}</p>
                 </div>
